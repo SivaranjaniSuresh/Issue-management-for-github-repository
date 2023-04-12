c0dbe0a9ce01420df06de00e75d6a1a75a7c7c5e
KaranAgrawal1509 <90572559+KaranAgrawal1509@users.noreply.github.com>
2023-04-12 15:51:59 -0400

    Remove Nodes and Relationship Functions added

--- neo4j_helper_functions.py
+++ neo4j_helper_functions.py
@@ -281,3 +281,255 @@ class Neo4jGitHub:
             issue_number=issue_number,
             label_type=label_type,
         )
+
+    ####################################################################################################################################
+    ### Remove Nodes
+    ####################################################################################################################################
+
+    def remove_issue(self, owner, repo_name, issue_number):
+        try:
+            with self.driver.session(database="neo4j") as session:
+                session.execute_write(
+                    self._remove_issue,
+                    owner,
+                    repo_name,
+                    issue_number,
+                )
+        except ServiceUnavailable as e:
+            logging.error(f"ServiceUnavailable exception occurred: {e}")
+
+    @staticmethod
+    def _remove_issue(tx, owner, repo_name, issue_number):
+        query = (
+            "MATCH (i:Issue {Owner: $owner, RepoName: $repo_name, IssueNumber: $issue_number}) "
+            "DETACH DELETE i"
+        )
+        tx.run(
+            query,
+            owner=owner,
+            repo_name=repo_name,
+            issue_number=issue_number,
+        )
+
+    def remove_user(self, github_id):
+        try:
+            with self.driver.session(database="neo4j") as session:
+                session.execute_write(self._remove_user, github_id)
+        except ServiceUnavailable as e:
+            logging.error(f"ServiceUnavailable exception occurred: {e}")
+
+    @staticmethod
+    def _remove_user(tx, github_id):
+        query = "MATCH (u:User {GithubId: $github_id}) DETACH DELETE u"
+        tx.run(query, github_id=github_id)
+
+    def remove_reaction(self, reaction_name):
+        try:
+            with self.driver.session(database="neo4j") as session:
+                session.execute_write(self._remove_reaction, reaction_name)
+        except ServiceUnavailable as e:
+            logging.error(f"ServiceUnavailable exception occurred: {e}")
+
+    @staticmethod
+    def _remove_reaction(tx, reaction_name):
+        query = "MATCH (r:Reaction {ReactionName: $reaction_name}) DETACH DELETE r"
+        tx.run(query, reaction_name=reaction_name)
+
+    def remove_label(self, label_type):
+        try:
+            with self.driver.session(database="neo4j") as session:
+                session.execute_write(self._remove_label, label_type)
+        except ServiceUnavailable as e:
+            logging.error(f"ServiceUnavailable exception occurred: {e}")
+
+    @staticmethod
+    def _remove_label(tx, label_type):
+        query = "MATCH (l:Label {LabelType: $label_type}) DETACH DELETE l"
+        tx.run(query, label_type=label_type)
+
+    def remove_milestone(self, milestone_type):
+        try:
+            with self.driver.session(database="neo4j") as session:
+                session.execute_write(self._remove_milestone, milestone_type)
+        except ServiceUnavailable as e:
+            logging.error(f"ServiceUnavailable exception occurred: {e}")
+
+    @staticmethod
+    def _remove_milestone(tx, milestone_type):
+        query = "MATCH (m:Milestone {MilestoneType: $milestone_type}) DETACH DELETE m"
+        tx.run(query, milestone_type=milestone_type)
+
+    ####################################################################################################################################
+    ### Remove Relationships
+    ####################################################################################################################################
+
+    def remove_issue_creator_relationship(
+        self, user_github_id, issue_owner, issue_repo_name, issue_number
+    ):
+        try:
+            with self.driver.session(database="neo4j") as session:
+                session.execute_write(
+                    self._remove_issue_creator_relationship,
+                    user_github_id,
+                    issue_owner,
+                    issue_repo_name,
+                    issue_number,
+                )
+        except ServiceUnavailable as e:
+            logging.error(f"ServiceUnavailable exception occurred: {e}")
+
+    @staticmethod
+    def _remove_issue_creator_relationship(
+        tx, user_github_id, issue_owner, issue_repo_name, issue_number
+    ):
+        query = (
+            "MATCH (u:User {GithubId: $user_github_id})-[rel:CREATED]->(i:Issue {Owner: $issue_owner, RepoName: $issue_repo_name, IssueNumber: $issue_number}) "
+            "DELETE rel"
+        )
+        tx.run(
+            query,
+            user_github_id=user_github_id,
+            issue_owner=issue_owner,
+            issue_repo_name=issue_repo_name,
+            issue_number=issue_number,
+        )
+
+    def remove_issue_assigner_relationship(
+        self,
+        assigner_github_id,
+        assignee_github_id,
+        issue_owner,
+        issue_repo_name,
+        issue_number,
+    ):
+        try:
+            with self.driver.session(database="neo4j") as session:
+                session.execute_write(
+                    self._remove_issue_assigner_relationship,
+                    assigner_github_id,
+                    assignee_github_id,
+                    issue_owner,
+                    issue_repo_name,
+                    issue_number,
+                )
+        except ServiceUnavailable as e:
+            logging.error(f"ServiceUnavailable exception occurred: {e}")
+
+    @staticmethod
+    def _remove_issue_assigner_relationship(
+        tx,
+        assigner_github_id,
+        assignee_github_id,
+        issue_owner,
+        issue_repo_name,
+        issue_number,
+    ):
+        query = (
+            "MATCH (u1:User {GithubId: $assigner_github_id})-[rel:ASSIGNED_ISSUE]->(u2:User {GithubId: $assignee_github_id})-[:ASSIGNED_TO]->(i:Issue {Owner: $issue_owner, RepoName: $issue_repo_name, IssueNumber: $issue_number}) "
+            "DELETE rel"
+        )
+        tx.run(
+            query,
+            assigner_github_id=assigner_github_id,
+            assignee_github_id=assignee_github_id,
+            issue_owner=issue_owner,
+            issue_repo_name=issue_repo_name,
+            issue_number=issue_number,
+        )
+
+    def remove_user_reaction_relationship(
+        self, github_id, issue_owner, issue_repo_name, issue_number, reaction_name
+    ):
+        try:
+            with self.driver.session(database="neo4j") as session:
+                session.execute_write(
+                    self._remove_user_reaction_relationship,
+                    github_id,
+                    issue_owner,
+                    issue_repo_name,
+                    issue_number,
+                    reaction_name,
+                )
+        except ServiceUnavailable as e:
+            logging.error(f"ServiceUnavailable exception occurred: {e}")
+
+    @staticmethod
+    def _remove_user_reaction_relationship(
+        tx, github_id, issue_owner, issue_repo_name, issue_number, reaction_name
+    ):
+        query = (
+            "MATCH (u:User {GithubId: $github_id})-[rel:REACTED]->(r:Reaction {ReactionName: $reaction_name})-[:REACTION_ON]->(i:Issue {Owner: $issue_owner, RepoName: $issue_repo_name, IssueNumber: $issue_number}) "
+            "DELETE rel"
+        )
+        tx.run(
+            query,
+            github_id=github_id,
+            issue_owner=issue_owner,
+            issue_repo_name=issue_repo_name,
+            issue_number=issue_number,
+            reaction_name=reaction_name,
+        )
+
+    def remove_user_milestone_relationship(
+        self, github_id, issue_owner, issue_repo_name, issue_number, milestone_type
+    ):
+        try:
+            with self.driver.session(database="neo4j") as session:
+                session.execute_write(
+                    self._remove_user_milestone_relationship,
+                    github_id,
+                    issue_owner,
+                    issue_repo_name,
+                    issue_number,
+                    milestone_type,
+                )
+        except ServiceUnavailable as e:
+            logging.error(f"ServiceUnavailable exception occurred: {e}")
+
+    @staticmethod
+    def _remove_user_milestone_relationship(
+        tx, github_id, issue_owner, issue_repo_name, issue_number, milestone_type
+    ):
+        query = (
+            "MATCH (u:User {GithubId: $github_id})-[rel:ADDED_MILESTONE]->(m:Milestone {MilestoneType: $milestone_type})-[:MILESTONE_ON]->(i:Issue {Owner: $issue_owner, RepoName: $issue_repo_name, IssueNumber: $issue_number}) "
+            "DELETE rel"
+        )
+        tx.run(
+            query,
+            github_id=github_id,
+            issue_owner=issue_owner,
+            issue_repo_name=issue_repo_name,
+            issue_number=issue_number,
+            milestone_type=milestone_type,
+        )
+
+    def remove_issue_label_relationship(
+        self, issue_owner, issue_repo_name, issue_number, label_type
+    ):
+        try:
+            with self.driver.session(database="neo4j") as session:
+                session.execute_write(
+                    self._remove_issue_label_relationship,
+                    issue_owner,
+                    issue_repo_name,
+                    issue_number,
+                    label_type,
+                )
+        except ServiceUnavailable as e:
+            logging.error(f"ServiceUnavailable exception occurred: {e}")
+
+    @staticmethod
+    def _remove_issue_label_relationship(
+        tx, issue_owner, issue_repo_name, issue_number, label_type
+    ):
+        query = (
+            "MATCH (i:Issue {Owner: $issue_owner, RepoName: $issue_repo_name, IssueNumber: $issue_number})-[rel:HAS_LABEL]->(l:Label {LabelType: $label_type}) "
+            "DELETE rel"
+        )
+        tx.run(
+            query,
+            issue_owner=issue_owner,
+            issue_repo_name=issue_repo_name,
+            issue_number=issue_number,
+            label_type=label_type,
+        )
