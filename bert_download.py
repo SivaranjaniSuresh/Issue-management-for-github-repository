7ff45f3eb1168708f74755c6b408b1fcebee9e00
Lokeshwaran Venugopal Balamurugan <venugopalbalamurug.l@northeastern.edu>
2023-04-20 18:54:58 -0400

    OpenAI prompts added

--- bert_download.py
+++ bert_download.py
@@ -3,5 +3,5 @@ from transformers import BertModel, BertTokenizer
 model = BertModel.from_pretrained("bert-base-uncased")
 model.save_pretrained("./bert-base-uncased")
 
-# tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-# tokenizer.save_pretrained("./bert-base-uncased-tokenizer")
+#tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+#tokenizer.save_pretrained("./bert-base-uncased-tokenizer")
