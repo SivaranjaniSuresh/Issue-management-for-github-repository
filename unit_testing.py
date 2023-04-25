e3be1c9e833c856dc6fbd2107619330f64f279e8
Lokeshwaran Venugopal Balamurugan <venugopalbalamurug.l@northeastern.edu>
2023-04-25 15:40:03 -0400

    Continuous Integration Test

--- unit_testing.py
+++ unit_testing.py
@@ -1,7 +1,9 @@
 from unittest.mock import MagicMock
-
 import pytest
 import requests_mock
+import os
+from dotenv import load_dotenv
+import snowflake.connector
 
 from utils.core_helpers import (
     get_issue_comments,
@@ -9,6 +11,14 @@ from utils.core_helpers import (
     get_unique_owner_repo_pairs,
 )
 
+load_dotenv()
+
+SNOWFLAKE_ACCOUNT = os.environ.get("SNOWFLAKE_ACCOUNT")
+SNOWFLAKE_USER = os.environ.get("SNOWFLAKE_USER")
+SNOWFLAKE_PASSWORD = os.environ.get("SNOWFLAKE_PASSWORD")
+SNOWFLAKE_DATABASE = os.environ.get("SNOWFLAKE_DATABASE")
+SNOWFLAKE_SCHEMA = os.environ.get("SNOWFLAKE_SCHEMA")
+SNOWFLAKE_WAREHOUSE = os.environ.get("SNOWFLAKE_WAREHOUSE")
 
 def test_get_open_issues_success():
     owner = "test_owner"
@@ -73,7 +83,6 @@ def test_get_open_issues_invalid_token():
         result = get_open_issues(owner, repo, access_token, page, per_page)
         assert result == []
 
-
 def test_get_unique_owner_repo_pairs_success():
     mock_session = MagicMock()
     mock_result = MagicMock()
@@ -95,7 +104,6 @@ def test_get_unique_owner_repo_pairs_success():
         ("owner2", "repo2"),
         ("owner3", "repo3"),
     ]
-
 
 def test_get_unique_owner_repo_pairs_empty_result():
     mock_session = MagicMock()
@@ -147,6 +155,69 @@ def test_get_issue_comments_invalid_token():
 
         result = get_issue_comments(issue_url, access_token)
         assert result == []
+
+def get_issue_url(issue_number, repo_owner=None, repo_name=None):
+    conn = snowflake.connector.connect(
+    user=SNOWFLAKE_USER,
+    password=SNOWFLAKE_PASSWORD,
+    account=SNOWFLAKE_ACCOUNT,
+    warehouse=SNOWFLAKE_WAREHOUSE,
+    database=SNOWFLAKE_DATABASE,
+    schema=SNOWFLAKE_SCHEMA,
+    )
+    cursor = conn.cursor()
+    query = f"SELECT ISSUE_URL, TITLE FROM GITHUB_ISSUES.PUBLIC.ISSUES WHERE ID = '{issue_number}'"
+    result = cursor.execute(query)
+    row = result.fetchone()
+    cursor.close()
+    conn.close()
+    return (row[0], row[1]) if row else (None, None)
+
+# Test data setup
+test_data = [
+    {
+        "issue_number": 1647155662,
+        "repo_owner": "openai",
+        "repo_name": "openai-python",
+        "issue_url": "https://github.com/openai/openai-python/issues/358",
+        "title": "request_id is not work",
+    },
+    {
+        "issue_number": 1669196666,
+        "repo_owner": "twitter",
+        "repo_name": "the-algorithm",
+        "issue_url": "https://github.com/twitter/the-algorithm/issues/1784",
+        "title": 'Sound of video from "For You" tab plays after switching to "Following" tab in Twitter Android app',
+    },
+    {
+    "issue_number": 1665279685,
+    "repo_owner": "facebook",
+    "repo_name": "Rapid",
+    "issue_url": "https://github.com/facebook/Rapid/issues/914",
+    "title": "Selecting multiple alike elements, suddenly dissapear",
+    },
+]
+
+# Define the test function
+@pytest.mark.parametrize(
+    "test_data_item",
+    test_data,
+    ids=[
+        "Test with OpenAI issue",
+        "Test with Twitter issue",
+        "Test with Facebook issue",
+    ],
+)
+def test_get_issue_url(test_data_item):
+    issue_url, title = get_issue_url(test_data_item["issue_number"])
+    assert issue_url == test_data_item["issue_url"]
+    assert title == test_data_item["title"]
+
+# Test with non-existent issue number
+def test_get_issue_url_non_existent():
+    issue_url, title = get_issue_url(9999)
+    assert issue_url is None
+    assert title is None
 
 
 if __name__ == "__main__":
